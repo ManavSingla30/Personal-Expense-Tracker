4a6e4c8164005a4a7fd3ca6884c2032e9ddda906
Manav Singla <manavsingla@Manavs-MacBook-Air.local>
2025-10-18 20:12:21 +0530

    Commit

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ const app = express();
 
 mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/backend", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
